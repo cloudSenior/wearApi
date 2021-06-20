5eeba85e98342cbb1a9267ffdce34e72a043f65c
cloud <86138476+cloudSenior@users.noreply.github.com>
2021-06-20 06:07:56 +0300

    Update log.hh

--- include/log.hh
+++ include/log.hh
@@ -1,16 +1,11 @@
 #include <iostream>
 #include <type_traits>
-#include "types.hh"
-#include "debug.hh"
-
-#define _LOG_HH
-
 
 namespace wear {
 #ifdef _DEBUG_WARNING
 	template < class T > void warrning (T out_content)
 	{
-		static_assert (std::is_same< T, char >() || std::is_same< T, wear::wchar >(),
+		static_assert (std::is_same< T, char >() || std::is_same< T, wchar_t >(),
 					   "[Error] type this not char/wchar");
 		std::clog << T << std::endl;
 	}
@@ -18,7 +13,7 @@ namespace wear {
 #ifdef _DEBUG_ERROR
 	template < class T > void error (T out_content)
 	{
-		static_assert (std::is_same< T, char >() || std::is_same< T, wear::wchar >(),
+		static_assert (std::is_same< T, char >() || std::is_same< T,  wchar_t >(),
 					   "[Error] type this not char/wchar");
 		std::cerr << T << std::endl;
 	}
